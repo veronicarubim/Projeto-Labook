b2f474a2d7fd1a433ebcc4ee55669a049f270f85
veronicarubim <veronica.rubim97@gmail.com>
2023-02-23 17:02:12 -0300

    Arquivos de configuração e autenticação

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(3003, () => {
-    console.log("Servidor rodando na porta 3003");
-});
+app.listen(Number(process.env.PORT), () => {
+    console.log(`Servidor rodando na porta ${Number(process.env.PORT)}`)
+})
+
+app.use("/users", userRouter)
 
 // Endpoints 
 
